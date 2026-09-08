f307926bdb1d188b3a451b9137d51cc5f7eb2c27
Eliezer KIERA <kieraeliezer7@yahoo.fr>
2026-09-08 22:57:10 +0000

    Test API

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', function(){
+    $monTableau = ['nom' => 'Dupont', 'age' => 30];
+    
+    return response()->json($monTableau);
+});
